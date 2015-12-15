685f78751610975caf1923ac48e82b4909d0a1a8
ayhankuru <ayhankuru@yandex.com.tr>
2015-12-15 15:54:36 +0200

    working

--- example.js
+++ example.js
@@ -8,7 +8,6 @@ var sevenx = new Seven();
 //setTimeout(function () {
 //  throw new Error('Oh No!');
 //}, 2000);
-
-sevenx.error(new Error('Hey Hoy2'),{user:"ayhankuru",id:124543});
+//sevenx.error(new Error('Hey Hoy2'),{user:"ayhankuru",id:124543});
 //sevenx.error(new Error('Hey Hoy'));
 //sevenx.error(new Error('Hey Hoy'));
